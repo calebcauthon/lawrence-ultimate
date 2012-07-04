a7cdcea40115b6bc1c809c13dd49fe37ece81ca9
Caleb Cauthon <calebcauthon@gmail.com>
2012-07-04 12:26:02 -0500

    Revert "added Airbrake"
    This reverts commit 4812f2c7feed13c1b0c41074142bbdf84cbecbdc.

--- web.rb
+++ web.rb
@@ -7,15 +7,8 @@ require 'mongo'
 require 'mail'
 require 'csv'
 require 'smoke_monster'
-require 'airbrake'
 
 enable :sessions
-
-Airbrake.configure do |config|
-  config.api_key = '665982ab7514b4ed09a2bf65c3110c7f'
-end
-
-use Airbrake::Rack
 
 
 class Team
@@ -69,6 +62,7 @@ end
 
 get '/standings' do
 	@teams = Array.new
+	
 	
 	db = Mongo::Connection.new('ds033897.mongolab.com', 33897).db('heroku_app2357454')
 	db.authenticate('ccauthon', 'ccauthon')   
